e6b4a1a8384787af83e111178c8ad4f943ce65e2
Jiuyang Liu <liu@jiuyang.me>
2025-07-09 13:19:09 -0400

    prototype JsonBackend ocaml structure

--- nix/overlay.nix
+++ nix/overlay.nix
@@ -6,4 +6,6 @@ rec {
   circt-llvm = final.callPackage "${final.path}/pkgs/by-name/ci/circt/circt-llvm.nix" { };
 
   asl-llvm = circt-llvm.dev;
+
+  asl-json-backend = final.callPackage ./pkgs/asl-json-backend.nix { };
 }
